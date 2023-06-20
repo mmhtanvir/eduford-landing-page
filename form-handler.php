cd6af2f467acca37919e558f04ecb63ee4da8212
MahmudulHasanTanvir <82567711+MahmudulHasanTanvir@users.noreply.github.com>
2023-06-20 23:07:54 +0600

    1st commit

---
+++ form-handler.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$to = "mahamudulhasantanvir16@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@eduford-landing-page.herokuapp.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:contact.html");
+?>
